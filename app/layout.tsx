995d87b97721bb9fbef426ae3059fef993b45dd1
emmanuel <emmanuelowouko@gmail.com>
2025-02-27 09:55:19 +0000

    fix google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,23 +25,32 @@ export default function RootLayout({
       <head>
         {/* Google Analytics */}
         <Script
-          strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=G-HRXPV17YNY`}
-        />
-        <Script
-          id="google-analytics"
+          id="google-analytics-init"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-XXXXXXXXXX', {
+            `,
+          }}
+        />
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-HRXPV17YNY"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              gtag('config', 'G-HRXPV17YNY', {
                 page_path: window.location.pathname,
               });
             `,
           }}
         />
+
       </head>
       <body className={`${inter.className} overflow-x-hidden`}>
         <DynamicNavbar />
