e6c18a9132a6a9428e385c73856ea5fb8aef6efe
emmanuel <emmanuelowouko@gmail.com>
2025-03-01 19:54:57 +0000

    setup sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://lexpertpro.com', // Your website URL
+  generateRobotsTxt: true, // Automatically generate robots.txt
+}
