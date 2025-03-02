cfec82ca88e6277e29d58d55eb91ee358e93bde5
emmanuel <emmanuelowouko@gmail.com>
2025-03-02 02:38:44 +0000

    fix bug

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -5,7 +5,6 @@ import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import AnimatedSection from "./AnimatedSection";
-import Link from "next/link";
 
 // Define types for our pricing data
 interface PricingPlan {
